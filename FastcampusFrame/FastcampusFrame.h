b9aacfdb2f146831a212bc9dd193917486e3bd28
Suyeol Jeon <devxoul@gmail.com>
2017-03-29 20:09:28 +0900

    Initial commit :)

---
+++ FastcampusFrame/FastcampusFrame.h
@@ -0,0 +1,19 @@
+//
+//  FastcampusFrame.h
+//  FastcampusFrame
+//
+//  Created by Suyeol Jeon on 29/03/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastcampusFrame.
+FOUNDATION_EXPORT double FastcampusFrameVersionNumber;
+
+//! Project version string for FastcampusFrame.
+FOUNDATION_EXPORT const unsigned char FastcampusFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastcampusFrame/PublicHeader.h>
+
+
